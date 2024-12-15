0ec3fa5a41cd1fdff689bf10a8ad6d12ff4fbf5c
Rui Caridade <rui@lineupnow.com>
2024-12-15 17:27:06 +0000

    Day 6 Part 1

--- day6.py
+++ day6.py
@@ -1,7 +1,11 @@
-import sys
+"""
+This is totally over-engineered, but I wanted to have some fun with it :)
+"""
+
+import os
 from collections import Counter
 from dataclasses import dataclass
-from time import sleep
+from time import sleep, time
 from typing import Literal
 
 TileKind = Literal["ground"] | Literal["obstacle"]
@@ -42,12 +46,31 @@ class World:
     tiles: list[Tile]
     path: list[Tile]
 
+    _last_tile: Tile | None
+    _buffer: list[list[str]]
+
     def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
         self.tiles = [Tile(x, y, "ground") for y in range(height) for x in range(width)]
         self.path = []
         self.guard = Guard(0, 0, "north")
+
+        self._last_tile = None
+        self._buffer = [
+            ["╔"] + ["═" for _ in range(width)] + ["╗"],
+            *(["║"] + [" " for _ in range(width)] + ["║"] for _ in range(height)),
+            ["╚"] + ["═" for _ in range(width)] + ["╝"],
+        ]
+
+    def load(self) -> None:
+        for y in range(self.height):
+            for x in range(self.width):
+                tile = self.get_tile(x, y)
+                if tile.kind == "obstacle":
+                    self._buffer[y + 1][x + 1] = "█"
+                else:
+                    self._buffer[y + 1][x + 1] = " "
 
     def update(self) -> bool:
         try:
@@ -57,25 +80,8 @@ class World:
             return False
 
     def render(self) -> None:
-        lines: list[str] = []
-        lines.append("".join(["╔"] + ["═" for _ in range(self.width)] + ["╗"]))
-        for y in range(self.height):
-            line: str = "║"
-            for x in range(self.width):
-                tile = self.get_tile(x, y)
-                if tile.kind == "obstacle":
-                    line += "█"
-                else:
-                    line += " "
-            line += "║"
-
-            lines.append(line)
-
-        lines.append("".join(["╚"] + ["═" for _ in range(self.width)] + ["╝"]))
-
-        for tile in self.path:
-            line = lines[tile.y + 1]
-            lines[tile.y + 1] = line[: tile.x + 1] + "░" + line[tile.x + 2 :]
+        if self._last_tile and self._last_tile.kind == "ground":
+            self._buffer[self._last_tile.y + 1][self._last_tile.x + 1] = "░"
 
         guard_symbol = "O"
         match self.guard.orientation:
@@ -88,16 +94,26 @@ class World:
             case "west":
                 guard_symbol = "◄"
 
-        line = lines[self.guard.y + 1]
-        lines[self.guard.y + 1] = (
-            line[: self.guard.x + 1] + guard_symbol + line[self.guard.x + 2 :]
+        self._buffer[self.guard.y + 1][self.guard.x + 1] = guard_symbol
+
+        output = "\n".join(["".join(row) for row in self._buffer])
+        world_metadata = f"World: {self.width}x{self.height}"
+        guard_metadata = f"Guard: {self.guard}"
+        visited_tiles = f"Visited tiles: {len(self.visited_tiles)}"
+
+        os.system("clear")
+        print(
+            f"{output}"
+            f"\n\n{world_metadata}"
+            f"\n{guard_metadata}"
+            f"\n{visited_tiles}\n",
         )
 
-        output = "\n".join(lines)
-        sys.stdout.write(f"\r{output}")
-        sys.stdout.flush()
-
     def move_guard(self) -> None:
+        current_tile = self.get_tile(self.guard.x, self.guard.y)
+        if not self.path:
+            self.path.append(current_tile)
+
         try:
             next_tile = self.get_next_tile()
         except IndexError:
@@ -124,6 +140,7 @@ class World:
         self.guard.x = next_tile.x
         self.guard.y = next_tile.y
         self.path.append(next_tile)
+        self._last_tile = current_tile
 
     def get_next_tile(self) -> Tile:
         match self.guard.orientation:
@@ -137,12 +154,18 @@ class World:
                 return self.get_tile(self.guard.x - 1, self.guard.y)
 
     def get_tile(self, x: int, y: int) -> Tile:
+        if x < 0 or x >= self.width or y < 0 or y >= self.height:
+            raise IndexError("Tile is out of bounds!")
+
         return self.tiles[y * self.width + x]
 
     @property
     def visited_tiles(self) -> list[Tile]:
         tiles = Counter([tile for tile in self.path if tile.kind == "ground"])
         return list(tiles.keys())
+
+    def __str__(self) -> str:
+        return f"World({self.width}, {self.height})"
 
 
 def parse_world() -> World:
@@ -154,14 +177,15 @@ def parse_world() -> World:
     }
 
     with open("inputs/day6.txt") as f:
-        lines = f.readlines()
+        content = f.read()
+        lines = [line.strip() for line in content.split("\n") if line.strip()]
+
         world = World(
             height=len(lines),
             width=len(lines[0]),
         )
-
         for y, line in enumerate(lines):
-            for x, c in enumerate(line):
+            for x, c in enumerate(line.strip()):
                 tile = world.get_tile(int(x), int(y))
 
                 match c:
@@ -178,18 +202,21 @@ def parse_world() -> World:
 
 def solve() -> None:
     world = parse_world()
+    world.load()
+
+    start_time = time()
+    tick_rate = 360
+    sleep_seconds = 1 / tick_rate
 
     is_running = True
-    ticks = 0
     while is_running:
         is_running = world.update()
         world.render()
-        ticks += 1
-        sleep(0.1)
-
-    print(f"Completed in {ticks} ticks!")
+        sleep(sleep_seconds)
 
     # debug_world(world)
+    end_time = time()
+    print(f"Execution time: {end_time - start_time:.2f} seconds")
     print("Part 1:", len(world.visited_tiles))
 
 
